2c5b63019da1853983c08533a1c1f4c9a0b2745c
Florian Best <best@univention.de>
2019-02-17 12:45:55 +0100

    Implement object oriented strucutre

--- lfsr/__init__.py
+++ lfsr/__init__.py
@@ -6,6 +6,7 @@ https://www.xilinx.com/support/documentation/application_notes/xapp210.pdf
 https://users.ece.cmu.edu/~koopman/lfsr/
 https://en.wikipedia.org/wiki/Linear-feedback_shift_register
 """
+import itertools
 
 
 def lfsr_if(poly, init=1):
@@ -146,3 +147,80 @@ def measure_period(cntr):
             break
         buf.append(x)
     return len(buf)
+
+
+class LFSR(object):
+
+    @property
+    def taps(self):
+        return self.poly_to_taps(self.poly)
+
+    def __init__(self, taps, seed):
+        self.__iter = None
+        if not isinstance(taps, int):
+            taps = self.taps_to_poly(taps)
+        self.poly = taps
+        self.seed = seed
+        self.key_length = max(self.taps)
+        self.state = seed
+        self.lfsr = self.algorithm(self.poly, self.seed)
+
+    def poly_to_taps(self, poly):
+        return list(reversed([i for i, x in enumerate(reversed(bin(poly)[2:])) if x == '1']))
+
+    def taps_to_poly(self, taps):
+        return taps_to_poly(taps)
+
+    def get_period(self):
+        return measure_period(type(self)(self.poly, self.seed).lfsr)
+
+    def getchar(self):
+        return chr(self.getbyte())
+
+    def getbyte(self):
+        return int(''.join(map(str, self.getbits(8))), 2)
+
+    def getbits(self, num):
+        return list(itertools.islice(self, num))
+
+    def getbit(self):
+        return next(self)
+
+    def __call__(self):
+        while True:
+            data = next(self.lfsr)
+            for x in bin(data)[2:].zfill(self.key_length):
+                yield int(x)
+
+    def __iter__(self):
+        return self()
+
+    def __next__(self):
+        if self.__iter is None:
+            self.__iter = self.__iter__()
+        try:
+            return next(self.__iter)
+        except StopIteration:
+            self.__iter = None
+            raise
+    next = __next__
+
+    def __repr__(self):
+        return '<%s(seed=0x%X (%dbit), polynomial=0x%X, state=0x%X)>' % (type(self).__name__, self.key_length, self.seed, self.poly, self.state)
+
+
+class Galois(LFSR):
+    @property
+    def algorithm(self):
+        def _fixed(poly, seed):
+            s = lfsr_if(poly, seed)
+            assert next(s) == seed
+            while True:
+                yield next(s)
+        return _fixed
+
+
+class Fibonacci(LFSR):
+    @property
+    def algorithm(self):
+        return lfsr_ef
