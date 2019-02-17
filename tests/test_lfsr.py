dfe1148eb5d37e847a9ae04156250f01df37b521
Florian Best <best@univention.de>
2019-02-17 11:10:10 +0100

    Code cleanup tests: PEP8

--- tests/test_lfsr.py
+++ tests/test_lfsr.py
@@ -1,22 +1,22 @@
-from lfsr import *
-from lfsr.db import *
+from lfsr import lfsr_if, lfsr_ef, taps_to_poly, measure_period
+from lfsr.db import max_len_lfsr_min_taps
+
 
 def test_max_len_lfsr_if():
-    for n in range(2,10):
+    for n in range(2, 10):
         init = 1
         taps = max_len_lfsr_min_taps[n]
         poly = taps_to_poly(taps)
         prng = lfsr_if(poly, init)
         period = measure_period(prng)
-        assert(period == 2**n-1)
+        assert(period == 2**n - 1)
+
 
 def test_max_len_lfsr_ef():
-    for n in range(2,10):
+    for n in range(2, 10):
         init = 1
         taps = max_len_lfsr_min_taps[n]
         poly = taps_to_poly(taps)
         prng = lfsr_ef(poly, init)
         period = measure_period(prng)
-        assert(period == 2**n-1)
-
-
+        assert(period == 2**n - 1)
